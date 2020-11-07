05f1ab7773c5f40b40e8e85501036932cdf7d41c
Lama <lamayousef1997@gmail.com>
2020-11-07 15:20:18 -0800

    last

--- app/src/main/java/com/magsood/medappuser/Activity/MapsActivity.java
+++ app/src/main/java/com/magsood/medappuser/Activity/MapsActivity.java
@@ -1,20 +1,35 @@
 package com.magsood.medappuser.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Point;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.Projection;
+import com.google.android.gms.maps.model.Marker;
 import com.magsood.medappuser.R;
 import com.magsood.medappuser.Service.RequestService;
 import com.magsood.medappuser.Utils.ToolbarClass;
@@ -25,9 +40,21 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+import static androidx.constraintlayout.motion.widget.Debug.getLocation;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback ,GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleMap.OnMarkerDragListener,
+        GoogleMap.OnMapLongClickListener,
+        GoogleMap.OnMarkerClickListener,
+        View.OnClickListener{
 
     private GoogleMap mMap;
+    private GoogleApiClient googleApiClient;
+    private double longitude;
+    private double latitude;
+    private static final String TAG = "MapsActivity";
+    private FusedLocationProviderClient fusedLocationClient;
 
 
 
@@ -37,15 +64,24 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         init();
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
 
-        ( (LinearLayout)(findViewById(R.id.ic_back))).setOnClickListener(new View.OnClickListener() {
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        ((LinearLayout) (findViewById(R.id.ic_back))).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext() ,MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
         });
@@ -57,8 +93,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onClick(View view) {
 //                showDialog();
-             //   startActivity(new Intent(MapsActivity.this,SendRequest.class));
-             sendReq();
+                //   startActivity(new Intent(MapsActivity.this,SendRequest.class));
+                sendReq();
 
 
             }
@@ -74,32 +110,31 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         String lang = bundle.getString("dropLng");
         String lat = bundle.getString("dropLat");
         RequestService requestService = new RequestService();
-        requestService.sendRequest(this,medicineID,amount,lang,lat);
+        requestService.sendRequest(this, medicineID, amount, lang, lat);
     }
-
 
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         Bundle bundle = getIntent().getExtras();
-if(bundle!=null) {
-    String lang = bundle.getString("dropLng");
-    String lat = bundle.getString("dropLat");
-    // Add a marker in Sydney and move the camera
-    LatLng sydney = new LatLng(Double.parseDouble(lang), Double.parseDouble(lat));
-    mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Pharmacy"));
-    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12.0f));
-}
-else{
-    LatLng sydney = new LatLng(100f, 100f);
-    mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Pharmacy"));
-    mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12.0f));
+        if (bundle != null) {
+            String lang = bundle.getString("dropLng");
+            String lat = bundle.getString("dropLat");
+            // Add a marker in Sydney and move the camera
+            LatLng sydney = new LatLng(Double.parseDouble(lang), Double.parseDouble(lat));
+            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Pharmacy"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12.0f));
+        } else {
+//            LatLng sydney = new LatLng(100f, 100f);
+//            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Pharmacy"));
+//            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12.0f));
+            getCurrentLocation();
 
 
-}
+        }
 
 //        int zoom = (int)mMap.getCameraPosition().zoom;
 //        CameraUpdate cu = CameraUpdateFactory.newLatLngZoom(new
@@ -109,6 +144,84 @@ else{
 
 
     }
+
+    @SuppressLint("NewApi")
+    private void getCurrentLocation() {
+        mMap.clear();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            if ( Build.VERSION.SDK_INT >= 23){
+//                if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+//                        PackageManager.PERMISSION_GRANTED  ){
+//                    requestPermissions(new String[]{
+//                                    android.Manifest.permission.ACCESS_FINE_LOCATION},
+//                            REQUEST_CODE_ASK_PERMISSIONS);
+//                    return ;
+//                }
+//            }
+
+                requestPermissions(new String[]{
+                                android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+
+
+
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.e(TAG,"i stopp in premission");
+            return;
+        }
+        FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+//        Location location;
+//        location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
+            if (location != null) {
+                // Logic to handle location object
+
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+
+                //moving the map to location
+                moveMap();
+            } else {
+
+
+                //Log.e(TAG,location.toString());
+                // Handle null case or Request periodic location update https://developer.android.com/training/location/receive-location-updates
+            }
+        });
+//        if (location != null) {
+//            //Getting longitude and latitude
+//            longitude = location.getLongitude();
+//            latitude = location.getLatitude();
+//
+//            //moving the map to location
+//            moveMap();
+//        }
+    }
+    private void moveMap() {
+        /**
+         * Creating the latlng object to store lat, long coordinates
+         * adding marker to map
+         * move the camera with animation
+         */
+        LatLng latLng = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .draggable(true)
+                .title("Marker in India"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+
+    }
+
 
 
     AppCompatButton btn;
@@ -128,5 +241,87 @@ else{
 
         deleteDialog.show();
     }
+    @Override
+    public void onClick(View view) {
+        Log.v(TAG,"view click event");
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        getCurrentLocation();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        // mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(latLng).draggable(true));
+    }
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+        Toast.makeText(MapsActivity.this, "onMarkerDragStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+        Toast.makeText(MapsActivity.this, "onMarkerDrag", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+        // getting the Co-ordinates
+        latitude = marker.getPosition().latitude;
+        longitude = marker.getPosition().longitude;
+
+        //move to current position
+        moveMap();
+    }
+
+    @Override
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        Toast.makeText(MapsActivity.this, "onMarkerClick", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_ASK_PERMISSIONS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+            } else {
+                // Permission Denied
+                Toast.makeText(this, "your message", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
 
 }
